1142e6d08d4d9fc14abbad04c81ca745fc3019d0
marszm <mariusz.szmer@gmail.com>
2019-03-10 18:20:32 +0100

    jsp works fine

--- src/main/java/com/springsecuritydemoapp/demo/HomeController.java
+++ src/main/java/com/springsecuritydemoapp/demo/HomeController.java
@@ -1,12 +1,13 @@
 package com.springsecuritydemoapp.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
+
     @RequestMapping("/")
     public String home(){
-        return "home.jsp";
+        return "Hello World !!!";
     }
 }
