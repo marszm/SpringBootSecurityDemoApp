26e1523fec0aa8336469954c45ecbe8c597a6055
marszm <mariusz.szmer@gmail.com>
2019-03-11 19:45:11 +0100

    storing username and password in mysql database

--- src/main/java/com/springsecuritydemoapp/demo/AppSecurityConfig.java
+++ src/main/java/com/springsecuritydemoapp/demo/AppSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.springsecuritydemoapp.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -17,11 +21,22 @@ import java.util.List;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList();
-        users.add(User.withDefaultPasswordEncoder().username("mario").password("klop").roles("USER").build());
-        return new InMemoryUserDetailsManager(users);
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return daoAuthenticationProvider;
     }
+
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        List<UserDetails> users = new ArrayList();
+//        users.add(User.withDefaultPasswordEncoder().username("mario").password("oko").roles("USER").build());
+//        return new InMemoryUserDetailsManager(users);
+//    }
 }
